e128e99782668754a06d922a2abb245bbc4680ca
Nike3651 <52921389+Nike3651@users.noreply.github.com>
2020-11-19 11:07:39 +0200

    Added Login Model

---
+++ API/models/Login.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const LoginSchema = mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        Email: {
+            type: String, 
+            required: true, 
+            unique: true, 
+            match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        },
+        Password: {type: String, required: true}
+    });
+
+module.exports = mongoose.model('Login', LoginSchema);
